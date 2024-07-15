9f5ba088b6c76e4687679f52a71c65c36b50c764
pascariandrei <56169313+pascariandrei@users.noreply.github.com>
2024-07-15 12:55:53 +0300

    Update dio_request_inspector.dart

--- lib/dio_request_inspector.dart
+++ lib/dio_request_inspector.dart
@@ -35,4 +35,32 @@ class DioRequestInspector {
         MaterialPageRoute<dynamic>(
             builder: (_) => DashboardPage(password: password!)));
   }
+
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    if (kIsDebug) {
+      print("Request [${options.method}] ${options.path}");
+      print("Headers: ${options.headers}");
+      print("Data: ${options.data}");
+    }
+    handler.next(options); // Ensure this is called only once
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    if (kIsDebug) {
+      print("Response [${response.statusCode}] ${response.requestOptions.path}");
+      print("Data: ${response.data}");
+    }
+    handler.next(response); // Ensure this is called only once
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    if (kIsDebug) {
+      print("Error: ${err.message}");
+    }
+    handler.next(err); // Ensure this is called only once
+  }
 }
